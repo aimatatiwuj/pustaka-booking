d791d503591427f491b722dd5dd1fc6faeb1565c
tamiajuwita <juwitamia31@gmail.com>
2021-12-08 16:27:55 +0700

    menambahkan seluruh file *

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Tamia Juwita. Saya tinggal di daerah Curug. Hobi saya adalah menonton Youtube dan bermain games.";
+    }
+}
